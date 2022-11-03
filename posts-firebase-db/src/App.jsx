e0a2c6e36d4b6330902095937b0ba2edae1fb334
Syed Kariz Ali <karizali310@gmail.com>
2022-11-03 19:36:12 +0500

    another update

--- posts-firebase-db/src/App.jsx
+++ posts-firebase-db/src/App.jsx
@@ -1,29 +1,23 @@
-import './App.css';
 import { useState, useEffect } from "react";
-// import axios from 'axios';
+import './App.css';
+import axios from 'axios';
 import moment from 'moment';
 import { initializeApp } from "firebase/app";
-
-import {
-  getFirestore, collection,
-  addDoc, getDocs, doc,
-  onSnapshot, query, serverTimestamp,
-  orderBy, deleteDoc, updateDoc
-
-} from "firebase/firestore";
-
+import { getFirestore, collection, addDoc, getDocs, doc, onSnapshot, query } from "firebase/firestore";
+ 
 const firebaseConfig = {
-    apiKey: "AIzaSyC0qVw4uBTksLcf2gP-Eci_Q6jYQfBhAJo",
-    authDomain: "posts-firebase-database.firebaseapp.com",
-    projectId: "posts-firebase-database",
-    storageBucket: "posts-firebase-database.appspot.com",
-    messagingSenderId: "136045010484",
-    appId: "1:136045010484:web:cc6423cacaaf7077387461",
-    measurementId: "G-EEF0BB1Z3M"
+  apiKey: "AIzaSyC0qVw4uBTksLcf2gP-Eci_Q6jYQfBhAJo",
+  authDomain: "posts-firebase-database.firebaseapp.com",
+  projectId: "posts-firebase-database",
+  storageBucket: "posts-firebase-database.appspot.com",
+  messagingSenderId: "136045010484",
+  appId: "1:136045010484:web:cc6423cacaaf7077387461",
+  measurementId: "G-EEF0BB1Z3M"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+ 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
@@ -32,180 +26,97 @@ function App() {
 
   const [postText, setPostText] = useState("");
   const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [editing, setEditing] = useState({
-    editingId: null,
-    editingText: ""
-  });
+  // const [isLoading, setIsLoading] = useState(false);
+
+
 
 
   useEffect(() => {
-    const getData = async () => {
+      const getData = async() => {
       const querySnapshot = await getDocs(collection(db, "posts"));
-
       querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => `, doc.data());
-
-        setPosts((prev) => {
-          let newArray = [...prev, doc.data()];
+        console.log(`${doc.id} =>` , doc.data());
+        setPosts((prev)=> {
+          let newArray = [...prev, doc.data()]
           return newArray
         });
-
       });
     }
     // getData();
-
-    let unsubscribe = null;
-    const getRealtimeData = async () => {
-
-      const q = query(collection(db, "posts"), orderBy("createdOn", "desc"));
-
+    let unsubscribe;
+    const getRealTimeData = async () => {
+      const q = query(collection(db, "posts"));
       unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const posts = [];
-
-        querySnapshot.forEach((doc) => {
-          // posts.unshift(doc.data());
-          // posts.push(doc.data());
-
-          posts.push({ id: doc.id, ...doc.data() });
-
-        });
-
-        setPosts(posts);
-        console.log("posts: ", posts);
+      const allPosts = [];
+      querySnapshot.forEach((doc) => {
+        allPosts.push(doc.data());
       });
-
+      setPosts(allPosts);
+      console.log("Posts: ", allPosts);
+    });
     }
-    getRealtimeData();
+    getRealTimeData();
 
+
+    ///unsubscribtion
     return () => {
-      console.log("Cleanup function");
-      unsubscribe();
+    unsubscribe();
     }
-
+    
   }, [])
 
 
 
 
-  const savePost = async (e) => {
+  const createPost = async (e) => {
     e.preventDefault();
 
-    console.log("postText: ", postText);
-
+    console.log(postText);
     try {
-
       const docRef = await addDoc(collection(db, "posts"), {
         text: postText,
-        // createdOn: new Date().getTime(),
-        createdOn: serverTimestamp(),
+        createdOn: new Date().getTime(),
+        // postId: id
       });
       console.log("Document written with ID: ", docRef.id);
-
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-
-
-  }
-
-  const deletePost = async (postId) => {
-
-    console.log("postId: ", postId);
-
-    await deleteDoc(doc(db, "posts", postId));
-
-  }
-
-  const updatePost = async (e) => {
-    e.preventDefault();
-
-    await updateDoc(doc(db, "posts", editing.editingId), {
-      text: editing.editingText
-    });
-
-    setEditing({
-      editingId: null,
-      editingText: ""
-    })
-
+    
+    
   }
 
 
   return (
     <div>
 
-      <form onSubmit={savePost}>
-        <textarea
+      <form onSubmit={createPost}>
+        <input
           type="text"
-          placeholder="What's in your mind..."
+          placeholder="What's in your mind...."
           onChange={(e) => {
             setPostText(e.target.value)
           }}
         />
-        <br />
-        <button type="submit">Post</button>
+        <button type="submit">post</button>
       </form>
 
       <div>
-        {(isLoading) ? "loading..." : ""}
+        {/* {(isLoading) ? "loading..." : ""} */}
 
         {posts.map((eachPost, i) => (
           <div className="post" key={i}>
 
-            <h3
-              className="title"
-              href={eachPost?.url}
-              target="_blank" rel="noreferrer"
-            >
-              {(eachPost.id === editing.editingId) ?
-                <form onSubmit={updatePost}>
-
-                  <input
-                    type="text"
-                    value={editing.editingText}
-                    onChange={(e) => {
-                      setEditing({
-                        ...editing,
-                        editingText: e.target.value
-                      })
-                    }}
-                    placeholder="please enter updated value" />
-
-                  <button type="submit">Update</button>
-                </form>
-                :
-                eachPost?.text}
-            </h3>
+             
+            <h3>{eachPost?.text}</h3>
 
             <span>{
-              moment(
-                (eachPost?.createdOn?.seconds) ?
-                  eachPost?.createdOn?.seconds * 1000
-                  :
-                  undefined
-              )
+              moment(eachPost?.createdOn)
                 .format('Do MMMM, h:mm a')
             }</span>
 
-            <br />
-            <br />
-            <button onClick={() => {
 
-              deletePost(eachPost?.id)
-
-            }}>Delete</button>
-
-            {(editing.editingId === eachPost?.id) ? null :
-              <button onClick={() => {
-
-                setEditing({
-                  editingId: eachPost?.id,
-                  editingText: eachPost?.text
-                })
-
-              }} >Edit</button>
-            }
+             
 
           </div>
         ))}
