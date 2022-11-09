1446d4111b37023b82394ef1b5f7c4ef39cef598
Syed Kariz Ali <karizali310@gmail.com>
2022-11-09 21:36:47 +0500

    new update

--- posts-firebase-db/src/App.jsx
+++ posts-firebase-db/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import './App.css';
-// import axios from 'axios';
+import profilePic from "./images/profile.jpg";
+import axios from 'axios';
 import moment from 'moment';
 import { initializeApp } from "firebase/app";
 import {
    getFirestore, collection, addDoc, 
-   getDocs, doc, onSnapshot, query 
+   getDocs, doc, onSnapshot, query,
+   updateDoc, serverTimestamp, orderBy, limit,
+   deleteDoc 
   } from "firebase/firestore";
  
 const firebaseConfig = {
@@ -24,42 +27,29 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-
 function App() {
 
   const [postText, setPostText] = useState("");
   const [posts, setPosts] = useState([]);
+  const [editing, setEditing] = useState(null);
+  const [editingText, setEditingText] = useState(null);
+  const [image, setImage] = useState(null);
   // const [isLoading, setIsLoading] = useState(false);
 
-
-
-
   useEffect(() => {
-      const getData = async() => {
-      const querySnapshot = await getDocs(collection(db, "posts"));
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} =>` , doc.data());
-        setPosts((prev)=> {
-          let newArray = [...prev, doc.data()]
-          return newArray
-        });
-      });
-    }
-    // getData();
     let unsubscribe;
     const getRealTimeData = async () => {
-      const q = query(collection(db, "posts"));
+      const q = query(collection(db, "posts"), orderBy("createdOn", "desc"));
       unsubscribe = onSnapshot(q, (querySnapshot) => {
       const allPosts = [];
       querySnapshot.forEach((doc) => {
-        allPosts.push(doc.data());
+        allPosts.push({...doc.data(), id:doc.id});
       });
       setPosts(allPosts);
       console.log("Posts: ", allPosts);
     });
     }
     getRealTimeData();
-
 
     ///unsubscribtion
     return () => {
@@ -69,54 +59,143 @@ function App() {
   }, [])
 
 
-
-
   const createPost = async (e) => {
     e.preventDefault();
-
+    document.querySelector(".post-input").value = "";
     console.log(postText);
-    try {
-      const docRef = await addDoc(collection(db, "posts"), {
-        text: postText,
-        createdOn: new Date().getTime(),
-        // postId: id
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
+    if(postText === ""){
+        alert("Fill some text");
+    }else{
+      const cloudinaryData = new FormData();
+                cloudinaryData.append("file", image);
+                cloudinaryData.append("upload_preset", "create-posts images")
+                cloudinaryData.append("cloud_name", "dwfzx0tds")
+                axios.post(`https://api.cloudinary.com/v1_1/dwfzx0tds/image/upload`, cloudinaryData, { headers: { 'Content-Type': 'multipart/form-data' } })
+                    .then(async res => {
+                      try {
+                        const docRef = await addDoc(collection(db, "posts"), {
+                          text: postText,
+                          createdOn: serverTimestamp(),
+                          // createdOn: new Date().getTime(),
+                          // postId: id
+                          img: res?.data?.url
+                        });
+                        console.log("Document written with ID: ", docRef.id);
+                      } catch (e) {
+                        console.error("Error adding document: ", e);
+                      }
+                    })
+
+      
     }
     
-    
+  }
+  const deletePost = async (postId) => {
+    await deleteDoc(doc(db, "posts", postId));
+  };
+  const editPost = async (postId, updatedText) => {
+    await updateDoc(doc(db, "posts", postId), {
+      text: updatedText
+    });
+  };
+
+
+  const viewOptions = (e) => {
+    console.log(e)
+    if(document.querySelector(".options-box").style.display === "flex"){
+      document.querySelector(".options-box").style.display = "none"
+    }else{
+
+      document.querySelector(".options-box").style.display = "flex"
+    }
   }
 
-
   return (
-    <div>
-
+      <>
+    <div className="container">
+      {/* post create box */}
+      <div className="create-box">
       <form onSubmit={createPost}>
         <input
+        className="post-input"
           type="text"
           placeholder="What's in your mind...."
           onChange={(e) => {
             setPostText(e.target.value)
           }}
         />
+        <div className='button'>
+            <input type="file" id='image' onChange={(e) => { setImage(e.currentTarget.files[0]) }} />
+        </div>
         <button type="submit">post</button>
       </form>
+      </div>
+      {/* post create box end*/}
 
-      <div>
+      <div className="allposts-box">
         {/* {(isLoading) ? "loading..." : ""} */}
 
         {posts.map((eachPost, i) => (
           <div className="post" key={i}>
+            <div className="post-head">
+              <div className="post-info">
+              <img src={profilePic} alt="profile" />
+              <div className="post-head-texts">
+                <span className="user-name">Asharib Ali</span>
+                <span className="post-time">
+                  {
+                  moment((eachPost?.createdOn?.seconds) ? 
+                  eachPost?.createdOn?.seconds*1000 : 
+                  undefined)
+                    .format('Do MMMM, h:mm a')
+                  }
+                </span>
+              </div>
+              </div>
+              <span className="post-options"  onClick={() =>{viewOptions(eachPost)}}>g
+              <div className="options-box">
+                <span className="option" onClick={()=> {
+                deletePost(eachPost?.id);
+                }}>delete</span>
+                <span className="option" onClick={()=> {
+                const updatedState = posts.map(eachItem => {
+                  if(eachItem?.id === eachPost?.id){
+                    return {...eachItem, isEditing: true}
+                  }else{
+                    return eachItem
+                  }
+                })
+                setPosts(updatedState)
+                }}>edit</span>
+              </div>
+              </span>
+            </div>
+            <h3>{(eachPost.isEditing) ? 
+            <div>
+              <form >
+              <input type="text" value={editingText}
+              onChange={(e)=>{
+                setEditingText(e.target.value)
+              }}
+              />
 
-             
-            <h3>{eachPost?.text}</h3>
+              </form>
+              <button onClick={(postId)=> {
+                setEditing(postId)
+                // const updatedState = posts.map(eachItem => {
+                //   if(eachItem?.id === eachPost?.id){
+                //     return {...eachItem, isEditing: !eachItem.isEditing}
+                //   }else{
+                //     return eachItem
+                //   }
+                // })
+                // setPosts(updatedState)
+              }}>ok</button>
+              </div>:
+            eachPost?.text}
+            </h3>
+            <img className="post-img" src={eachPost?.img} alt="" />
 
-            <span>{
-              moment(eachPost?.createdOn)
-                .format('Do MMMM, h:mm a')
-            }</span>
 
 
              
@@ -126,6 +205,7 @@ function App() {
       </div>
 
     </div>
+    </>
   );
 }
 
